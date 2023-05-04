92bbc8d527313b31cbd6d76c99583e5b12f7cfae
alijumanov <jumanovsafarali@gmail.com>
2023-05-04 13:09:23 +0500

    add

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='Home'>
-        <Header />
+        <Header /> 
         <SliderProducts />
         <TopProducts />
         <SortProducts />
